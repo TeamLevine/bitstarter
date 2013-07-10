7f3dbad501cf44c42d7533fb5e1ba9a490f4dc95
Sam Levine <team.levine@gmail.com>
2013-07-10 17:18:19 +0000

    changed web.js to read from html file using readFileSync

--- web.js
+++ web.js
@@ -2,11 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(1000)
-buffer = fs.readFileSync('./index.html')
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8')
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(buf.toString);
 });
 
 var port = process.env.PORT || 5000;
