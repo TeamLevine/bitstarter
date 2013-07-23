41ce7e9891b9efd5f2c86cde565eeb850dd4110b
Sam Levine <team.levine@gmail.com>
2013-07-23 14:24:16 +0000

    pix

--- web.js
+++ web.js
@@ -9,6 +9,8 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
+app.use(express.static(__dirname + '/assets'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
